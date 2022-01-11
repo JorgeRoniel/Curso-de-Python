96221046213eb208065382ebb78bda0e9c5e3baf
JorgeRoniel <jorge.infor4g@gmail.com>
2022-01-11 16:17:17 -0300

    Primeiro commit

---
+++ seção_3/dir_e_help.py
@@ -0,0 +1,10 @@
+"""
+Dir -> Apresenta todos os atributos/propriedades e funçoes/metodos disponiveis 
+para determinado tipo de dado ou variavel
+
+dir(dado/valor)
+
+Help -> Apresenta como utilizar essas coisas acima
+
+help(dado/valor.função)
+"""
---
+++ seção_3/recebendo_dados.py
@@ -0,0 +1,9 @@
+name = input('What your name?')
+
+
+print('Welcome, {}'.format(name))
+
+age = input('How old are you? ')
+
+
+print(f'Oh, cool, you have {age} years old!')
